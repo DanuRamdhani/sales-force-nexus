7c2ff1e386660d1fe415566b2884f271e189e97e
Hinabil <139767756+Hinabil@users.noreply.github.com>
2025-11-18 12:48:39 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/style.css";
 import Dashboard from "./pages/Dashboard.jsx";
-import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </main>
   );
