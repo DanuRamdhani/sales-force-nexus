23fc923dbf065d2dec9993d4cb5f9c642a0b69a9
ACER ID <danurd.study@gmail.com>
2025-11-21 15:48:05 +0800

    feat: ui login, dashboard sales, detail costumer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./styles/style.css";
-import Dashboard from "./pages/Dashboard.jsx";
+import { Routes, Route } from "react-router-dom";
+import "./styles/App.css";
+import LoginPage from "./pages/LoginPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import LeadDetailPage from "./pages/LeadDetailPage.jsx";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
-    <main>
+    <div className="App">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <DashboardPage />
+          }
+        />
+        <Route
+          path="/leads/:leadId"
+          element={
+            <LeadDetailPage />
+          }
+        />
       </Routes>
-    </main>
+      <Toaster position="top-right" richColors />
+    </div>
   );
 }
 
